2cc72eca4c625e30cc1e62f021f48b5e5ee4c767
vdo162 <vdo162@gmail.com>
2021-04-20 07:52:30 +0300

    i am a FIXED!!!!

--- src/Components/Adaptive/Example/Example.js
+++ src/Components/Adaptive/Example/Example.js
@@ -4,13 +4,13 @@ import s from'./Example.module.css';
 export const Example = (props) => {
 	return <div>
 		<div>
-			<a href='/Exapmle/Fixed/Fixed.html' target="_blank">Fixed Web Design - фиксированная верстка</a>
+			<a href='/cheatsheet/Exapmle/Fixed/Fixed.html' target="_blank">Fixed Web Design - фиксированная верстка</a>
 		</div>
 		<div>
-			<a href='/Exapmle/Responsive/Responsive.html' target="_blank">Responsive Web Design (RWD) - отзывчивая (резиновая) верстка</a>
+			<a href='/cheatsheet/Exapmle/Responsive/Responsive.html' target="_blank">Responsive Web Design (RWD) - отзывчивая (резиновая) верстка</a>
 		</div>
 		<div>
-			<a href='/Exapmle/Responsive/Responsive.html' target="_blank">Adaptive Web Design (AWD) - адаптивная верстка</a>
+			<a href='/cheatsheet/Exapmle/Responsive/Responsive.html' target="_blank">Adaptive Web Design (AWD) - адаптивная верстка</a>
 		</div>
 	</div>
 }
